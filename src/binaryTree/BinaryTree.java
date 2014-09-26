7447664ac81b6be4c0ebec41687ffa87cdb74d3a
Dhyanam Vaidya <dhyanamvaidya@gmail.com>
2014-09-25 23:38:41 -0400

    First Commit
    Created a new BinaryTree class. More to follow.

---
+++ src/binaryTree/BinaryTree.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package binaryTree;
+
+/**
+ * @author dhyanamvaidya
+ *
+ */
+public class BinaryTree {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
