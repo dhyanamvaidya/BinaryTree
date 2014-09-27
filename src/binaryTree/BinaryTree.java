70038d3a938aa856ce561c9315dc09dcdbb0cd1b
Dhyanam Vaidya <dhyanamvaidya@gmail.com>
2014-09-27 15:24:58 -0400

    Add parse() and traversal methods, updated size()
    public static BinaryTree<String> parse(String treeDescription)
    Translates a String description of a tree into a Tree<String> object.
    The treeDescription has the form value(child child), where
    a value is any sequence of characters not containing parentheses or
    whitespace, and each child is either just a (String) value or is
    another treeDescription. Whitespace is used to separate values.
    For example, the String "one(two three(four(six seven) five)) six"
    becomes the Tree:
                                one
                                / \
                               /   \
                             two  three
                             		 /\
                             		/  \
                                four five
                                 /\
                                /  \
                              six seven
    Also, added a private helper method which tokenizes the given string
    so that it can be split into two separate child strings out of which
    two separate BinaryTree nodes can be made
    
    public void inOrderTraversal(), preOrderTraversal(), postOrderTraversal()

--- src/binaryTree/BinaryTree.java
+++ src/binaryTree/BinaryTree.java
@@ -3,7 +3,7 @@
  */
 package binaryTree;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 
 /**
  * @author dhyanamvaidya
@@ -78,9 +78,6 @@ public class BinaryTree<V> {
 	 * @return
 	 */
 	public int size() {
-		if(this == null)
-			return 0;
-		
 		int leftSubTreeSize = 0;
 		if(this.leftChild != null)
 			leftSubTreeSize = this.leftChild.size();
@@ -90,6 +87,42 @@ public class BinaryTree<V> {
 			rightSubTreeSize = this.rightChild.size();
 		
 		return leftSubTreeSize + rightSubTreeSize + 1;
+	}
+	
+	/**
+	 * Prints the nodes of this BinaryTree in an inOrder traversal
+	 * @return
+	 */
+	public void inOrderTraversal() {
+		if(this.leftChild != null)
+			this.leftChild.inOrderTraversal();
+		System.out.println(this.getValue());
+		if(this.rightChild != null)
+			this.rightChild.inOrderTraversal();
+	}
+	
+	/**
+	 * Prints the nodes of this BinaryTree in a preOrder traversal
+	 * @return
+	 */
+	public void preOrderTraversal() {
+		System.out.println(this.getValue());
+		if(this.leftChild != null)
+			this.leftChild.preOrderTraversal();
+		if(this.rightChild != null)
+			this.rightChild.preOrderTraversal();
+	}
+	
+	/**
+	 * Prints the nodes of this BinaryTree in a postOrder traversal
+	 * @return
+	 */
+	public void postOrderTraversal() {
+		if(this.leftChild != null)
+			this.leftChild.postOrderTraversal();
+		if(this.rightChild != null)
+			this.rightChild.postOrderTraversal();
+		System.out.println(this.getValue());
 	}
 	
 	/**
@@ -125,16 +158,126 @@ public class BinaryTree<V> {
 	}
 
 	/**
+	 * Translates a String description of a tree into a Tree<String> object. 
+	 * The treeDescription has the form value(child child), where 
+	 * a value is any sequence of characters not containing parentheses or 
+	 * whitespace, and each child is either just a (String) value or is 
+	 * another treeDescription. Whitespace is used to separate values.
+	 * For example, the String "one(two three(four(six seven) five)) six" 
+	 * becomes the Tree:
+	 *                            one
+	 *                            / \
+	 *                           /   \
+	 *                         two  three
+	 *                         		 /\
+	 *                         		/  \
+	 *                            four five
+	 *                             /\
+	 *                            /  \
+	 *                          six seven
+	 * @param treeDescription
+	 * @return
+	 */
+	public static BinaryTree<String> parse(String treeDescription) {
+		BinaryTree<String> root = null;
+		treeDescription = treeDescription.trim();
+		if(!treeDescription.contains("(")) {
+			root = new BinaryTree<String>(treeDescription.trim());
+			return root;
+		}
+		else {
+			int firstParenOccurrence = treeDescription.indexOf('(');
+			String rootValue = treeDescription.substring(0, firstParenOccurrence);
+			root = new BinaryTree<String>(rootValue.trim());
+			String childrenValue = treeDescription.substring(firstParenOccurrence+1, treeDescription.length()-1);
+			
+			ArrayList<String> tokenizedChildren = tokenizeChildren(childrenValue);
+			if(tokenizedChildren.size() == 1)
+				root.leftChild = parse(tokenizedChildren.get(0));
+			else if(tokenizedChildren.size() == 2)
+				root.leftChild = parse(tokenizedChildren.get(0));
+				root.rightChild = parse(tokenizedChildren.get(1));
+		}
+		return root;
+	}
+	
+	/**
+	 * Helper method which tokenizes the given string so that it can be split
+	 * into two separate child strings out of which two separate BinaryTree
+	 * node can be made
+	 * @param childrenValue
+	 * @return
+	 */
+	public static ArrayList<String> tokenizeChildren(String childrenValue) {
+		ArrayList<String> tokenizedChildren = new ArrayList<String>();
+		StringBuilder temp = new StringBuilder("");
+		int parenCounter = 0;
+		for(int i=0; i<childrenValue.length(); i++) {
+			if(childrenValue.charAt(i) == ' ' || i == childrenValue.length()-1) {
+				temp.append(childrenValue.charAt(i));
+				tokenizedChildren.add(temp.toString().trim());
+				temp.delete(0, temp.length());
+			}
+			else if(childrenValue.charAt(i) == '(') {
+				parenCounter += 1;
+				temp.append(childrenValue.charAt(i));
+				i += 1;
+				while(parenCounter > 0) {
+					if(childrenValue.charAt(i) == '(')
+						parenCounter += 1;
+					else if(childrenValue.charAt(i) == ')')
+						parenCounter -= 1;
+					temp.append(childrenValue.charAt(i));
+					i += 1;
+				}
+				tokenizedChildren.add(temp.toString().trim());
+				temp.delete(0, temp.length());
+			}
+			else {
+				temp.append(childrenValue.charAt(i));
+			}
+		}
+		return tokenizedChildren;
+	}
+
+	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		BinaryTree<String> bt1 = new BinaryTree<String>("2");
+		//checking contains
+		/*BinaryTree<String> bt1 = new BinaryTree<String>("2");
 		BinaryTree<String> bt2 = new BinaryTree<String>("1");
 		BinaryTree<String> bt3 = new BinaryTree<String>("1");
-		
 		bt1.setLeftChild(bt2);
 		bt2.setRightChild(bt3);
-		System.out.println(bt1.contains(bt3));
+		System.out.println(bt1.contains(bt3));*/
+		
+		//checking substring in parse
+		String treeDescription = "one(two three(four(six seven) five))";
+		/*int firstParenOccurrence = treeDescription.indexOf('(');
+		String rootValue = treeDescription.substring(0, firstParenOccurrence);
+		String childrenValue = treeDescription.substring(firstParenOccurrence+1, treeDescription.length()-1);*/
+		
+		//checking tokenized children
+		/*ArrayList<String> al = new ArrayList<String>();
+		al = tokenizeChildren(childrenValue);
+		for(String s : al)
+			System.out.println(s);*/
+		
+		//checking whether the tree has been built correctly or not
+		BinaryTree<String> root = BinaryTree.parse(treeDescription);
+		System.out.println("Size: "+root.size());
+		System.out.println("-------------PreOrder Traversal-------------");
+		root.preOrderTraversal();
+		System.out.println("-------------InOrder Traversal-------------");
+		root.inOrderTraversal();
+		System.out.println("-------------PostOrder Traversal-------------");
+		root.postOrderTraversal();
+		
+		//checking string builder delete method
+		/*StringBuilder sb = new StringBuilder("dhyanam");
+		System.out.println(sb.toString());
+		sb.delete(0, sb.length());	*/	
 	}
 
 }
