9d4b0d690bbc450a1dce3aefd24c81e834ce791c
Dhyanam Vaidya <dhyanamvaidya@gmail.com>
2014-09-26 20:44:33 -0400

    Add size() and clear() methods
    public int size() method returns the number of nodes including the root
    in the Binary Tree rooted at the node on which this method was called.
    
    public void clear() method removes all of the mappings from the node on
    which this method was called. It resets the BinaryTree with null values.

--- src/binaryTree/BinaryTree.java
+++ src/binaryTree/BinaryTree.java
@@ -3,6 +3,8 @@
  */
 package binaryTree;
 
+import java.util.HashMap;
+
 /**
  * @author dhyanamvaidya
  * Binary Tree is a node-based binary tree data structure which has the following properties:
@@ -16,7 +18,6 @@ public class BinaryTree<V> {
 	private V value;
 	private BinaryTree<V> leftChild;
 	private BinaryTree<V> rightChild;
-	
 	
 	/**
 	 * @return the value
@@ -60,17 +61,46 @@ public class BinaryTree<V> {
 		this.rightChild = rightChild;
 	}
 	
+	public BinaryTree(V value) {
+		this.value = value;
+		this.leftChild = null;
+		this.rightChild = null;
+	}
+	
 	public BinaryTree(V value, BinaryTree<V> leftChild, BinaryTree<V> rightChild) {
 		this.value = value;
 		this.leftChild = leftChild;
 		this.rightChild = rightChild;
+	}
+	
+	/**
+	 * Returns the number of nodes including the root in the Binary Tree rooted at this node
+	 * @return
+	 */
+	public int size() {
+		if(this == null)
+			return 0;
+		return this.leftChild.size() + this.rightChild.size() + 1;
+	}
+	
+	/**
+	 * Removes all of the mappings from this BinaryTree and resets the node with null values
+	 */
+	public void clear() {
+		this.value = null;
+		this.leftChild = null;
+		this.rightChild = null;
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+		HashMap<String, String> hm = new HashMap<String, String>();
+		hm.put("a", "1");
+		System.out.println(hm.size());
+		hm.clear();
+		System.out.println(hm.size());
 	}
 
 }
